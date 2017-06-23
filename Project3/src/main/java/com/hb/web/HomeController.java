84e5c3f82f1cd344aa8be3dfea88abdec0f93486
sungjun88 <sungjun88@gmail.com>
2017-06-23 16:57:19 +0900

    첫 커밋 메뉴바 레이아웃

---
+++ Project3/src/main/java/com/hb/web/HomeController.java
@@ -0,0 +1,56 @@
+package com.hb.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
+	public void main(Locale locale, Model model) {
+	}
+	
+	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	public void login(Locale locale, Model model) {
+	}
+	
+	
+	
+	@RequestMapping(method = RequestMethod.GET, value = "menu")  //인클루드용
+	   public String newItem(Model model) {
+	       return "menu";
+	   }
+	
+	
+	
+}
