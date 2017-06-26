d49098ad5669ff8f711a8f96ebfb1caf1b311b15
sungjun88 <sungjun88@gmail.com>
2017-06-26 21:33:27 +0900

    여러가지

---
+++ Project3/src/main/java/com/hb/mybatis/VO.java
@@ -0,0 +1,25 @@
+package com.hb.mybatis;
+
+public class VO {
+	private String id, pw;
+	public VO() {	}
+	public VO(String id, String pw) {
+		super();
+		this.id = id;
+		this.pw = pw;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+	
+}
